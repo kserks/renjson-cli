2929410f20fbf7d24b06d802061495504f440caa
kserks <riteres@list.ru>
2016-03-16 14:02:23 +0800

    test nw-builder && bin/vn

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-#! /usr/bin/env node
 'use strict'
 const main = require('./lib/main');
 const $ = require('./lib/config');
-if(module.parent){
-	$.browser = "nw";
-	module.exports =  main;
-}else{
 
-let args = process.argv.slice(2);
-
-args.map(function(key){
-		main[key]();	
-});
-}
+$.browser = "nw";
