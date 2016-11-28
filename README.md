8e89b668262577155e871d3e5d35d6fc8db18eaa
kserks <bakakasin@gmail.com>
2016-11-29 01:57:29 +0700

    prebuild ] Удалил лишнее после cli версии

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 #vnjson-utils
 ###MIT License
-###v0.4.5
-
-```batch
-vnjson init
-vnjson build
-vnjson run
-
-```
---
+++ index.js
@@ -0,0 +1,7 @@
+
+const
+  init                      = require('./lib/init'),
+  prebuild                  = require('./lib/prebuild');
+  
+module.exports = { init, prebuild };
+
