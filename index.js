384ac7851369c218d4260b025cc8ff5db9bc6cea
kserks <bakakasin@gmail.com>
2018-01-08 23:52:55 +0700

    Р–РёРІР°СЏ РїРµСЂРµР·Р°РіСЂСѓР·РєР°

--- index.js
+++ index.js
@@ -7,14 +7,37 @@ const log 						= require('hexo-log')();
 const vnjs 						= require('./vnjs');
 const projectDir 				= process.cwd();
 const scenesBuild               = require('./lib/scenes-build');
+const pluginsBuilder =  require('./lib/plugins-builder');
+const staticServer = require('./lib/server');
+
+const PORT = 9090;
+
 
 
 
 /**
  * Подключение модулей
  */
+//------------node_modules - сделать чтоб устанавливалось
+// в эту директорию
+//npm install <github username>/<github project>' 
+//--------------read-package-tree
+//--------------npmlog
 require('./modules.json').map( m=>require(m) );
 
+function run(){
+	/*
+		Запускаем сервер, собираем сцены и плагины
+
+	*/
+	 staticServer.run(join(projectDir, './build/tmp' ), PORT, projectDir);
+	 log.warn(`Open in your browser - http://localhost:${PORT}`);
+	 log.warn('Press Ctrl+C for kill process');	
+}
+
+/*
+    todo - очищать папку перед копированием, что бы мусора небыло
+*/
 function init(){
 
 /*
@@ -31,7 +54,7 @@ vnjs.emit('project-init', projectDir, function (err){
 	 * Первая сборка плагинов и сцены
 	 */
 	scenesBuild(projectDir/*,local*/);
-
+	pluginsBuilder(projectDir);
 	//buildPlugins();
 	/*
 	 * Событие сборки проекта
@@ -53,6 +76,7 @@ vnjs.emit('project-init', projectDir, function (err){
 
 
 module.exports = {
-	init
+	init,
+	run
 }
 
